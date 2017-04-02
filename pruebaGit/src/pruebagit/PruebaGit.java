dce6f42d8bce3d1c68e21dfe89839bc4a12c7050
emilio <emilio@192.168.1.129>
2017-04-03 01:50:44 +0200

    pruebaGit1

---
+++ pruebaGit/src/pruebagit/PruebaGit.java
@@ -0,0 +1,31 @@
+
+package pruebagit;
+
+
+public class PruebaGit {
+
+    
+     enum Color {AZUL,VERDE,AMARILLO};
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+  
+    
+    
+          Color casa;
+          
+          casa=Color.AZUL;
+          
+          
+          if (casa == Color.AZUL) {
+              System.out.println("LA casa es azul");
+        }else if(casa == Color.VERDE){
+              System.out.println("La casa es verde");
+          }else{
+              System.out.println("Amarillo");
+        }
+//f4444444444444444444
+    }
+    
+}
