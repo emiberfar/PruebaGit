c30bc619f05e45227b2d38165e2689e18714edd4
emilio <emilio@192.168.1.129>
2017-04-03 02:08:22 +0200

    pruebaGit3

--- pruebaGit/src/pruebagit/PruebaGit.java
+++ pruebaGit/src/pruebagit/PruebaGit.java
@@ -20,7 +20,9 @@ public class PruebaGit {
           
           if (casa == Color.AZUL) {
               System.out.println("LA casa es azul");
-        }else{
+        }else if(casa == Color.VERDE){
+              System.out.println("La casa es verde");
+          }else{
               System.out.println("Amarillo");
         }
 //f4444444444444444444
